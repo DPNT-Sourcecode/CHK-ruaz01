5ccb2179202a5e4067e37b11cf92720af499b49c
sbx_user1051 <sbx_user1051@169.254.30.90>
2024-12-20 23:50:26 +0000

    Fri Dec 20 23:50:26 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,9 +7,13 @@ import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        if(skus.trim().isEmpty()) {
+            return 0;
+        }
 //        throw new SolutionNotImplementedException();
         Map<Character, Integer> price = new HashMap<>();
         Map<Character, int[]> specialPrice = new HashMap<>();
+        Map<Character, Integer> items = new HashMap<>();
 
         price.put('A', 50);
         price.put('B', 30);
@@ -20,12 +24,30 @@ public class CheckoutSolution {
         specialPrice.put('B', new int[]{2, 45});
 
         for(Character ch: skus.toCharArray()) {
-            if(!price.containsKey(ch) || !Character.isDigit(ch)) {
+            if(!price.containsKey(ch)) {
                 return -1;
             }
+            int n = items.getOrDefault(ch, 0);
+            n++;
+            items.put(ch, n);
         }
 
+        int total = 0;
+        for(Character ch: items.keySet()) {
+            int number = items.get(ch);
+            if(specialPrice.containsKey(ch)) {
+                int offerMin = specialPrice.get(ch)[0];
+                int offerVal = specialPrice.get(ch)[1];
 
-       return -1;
+                int offerCount = number / offerMin;
+                total += offerCount * offerVal;
+                number = number % offerMin;
+            }
+            int regularPrice = price.get(ch);
+            total += regularPrice * number;
+        }
+
+        return total;
     }
 }
+
