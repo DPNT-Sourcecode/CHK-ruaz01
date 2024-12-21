c10c7b61dac0ddb7ed3057d788c517f2f2ef81fd
sbx_user1051 <sbx_user1051@169.254.30.90>
2024-12-21 00:12:01 +0000

    Sat Dec 21 00:12:01 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,26 +2,29 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CheckoutSolution {
+
+//    public Map<Character, Integer> reduceItem(Map)
+
     public Integer checkout(String skus) {
         if(skus.trim().isEmpty()) {
             return 0;
         }
 //        throw new SolutionNotImplementedException();
         Map<Character, Integer> price = new HashMap<>();
-        Map<Character, int[]> specialPrice = new HashMap<>();
+        Map<Character, Map<Integer, Integer>> specialPrice = new HashMap<>();
         Map<Character, Integer> items = new HashMap<>();
 
         price.put('A', 50);
         price.put('B', 30);
         price.put('C', 20);
         price.put('D', 15);
+        price.put('E', 40);
 
-        specialPrice.put('A', new int[]{3, 130});
-        specialPrice.put('B', new int[]{2, 45});
+        specialPrice.put('A', Map.of(3,130,5,200));
+        specialPrice.put('B', Map.of(2,45));
 
         for(Character ch: skus.toCharArray()) {
             if(!price.containsKey(ch)) {
@@ -32,16 +35,28 @@ public class CheckoutSolution {
             items.put(ch, n);
         }
 
+        int countE = items.getOrDefault('E', 0);
+        if(countE > 1) {
+            int countB = items.getOrDefault('B', 0);
+            countB = countB - (countE/2);
+            countB = Math.max(countB, 0);
+            items.put('B', countB);
+        }
+
+
         int total = 0;
         for(Character ch: items.keySet()) {
             int number = items.get(ch);
             if(specialPrice.containsKey(ch)) {
-                int offerMin = specialPrice.get(ch)[0];
-                int offerVal = specialPrice.get(ch)[1];
+                Map<Integer, Integer> offer = specialPrice.get(ch);
+                List<Integer> specialCounts = new java.util.ArrayList<>(offer.keySet().stream().toList());
+                specialCounts.sort(Comparator.reverseOrder());
+                for(int offerMin: specialCounts) {
+                    int offerCount = number / offerMin;
+                    total += offerCount * offer.get(offerMin);
+                    number = number % offerMin;
+                }
 
-                int offerCount = number / offerMin;
-                total += offerCount * offerVal;
-                number = number % offerMin;
             }
             int regularPrice = price.get(ch);
             total += regularPrice * number;
@@ -50,3 +65,4 @@ public class CheckoutSolution {
         return total;
     }
 }
+
